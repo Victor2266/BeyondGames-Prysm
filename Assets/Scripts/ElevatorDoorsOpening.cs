502b404440b16e60da5becf41cbbabc12872466f
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2021-04-25 08:00:42 -0400

    done adding elevator
    done adding elevator

---
+++ Assets/Scripts/ElevatorDoorsOpening.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class ElevatorDoorsOpening : MonoBehaviour
+{
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+
+    public float openPos;
+    public float closedPos;
+    public float speed;
+    private float posX;
+
+    private Rigidbody2D rb2d;
+    public bool opening;
+    private Transform door;
+    public bool shaking = false;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        door = rightDoor.GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "destroyoncontact")
+        {
+            if (Mathf.Abs(door.localPosition.x) - closedPos < 0.01f)
+            {
+                opening = true;
+            }
+        }
+        if (collision.tag == "Finish")
+        {
+            if (!shaking)
+            {
+                shaking = true;
+                CameraShaker.Instance.ShakeOnce(8, 3, .1f, .5f);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Mathf.Abs(door.localPosition.x) - openPos < 0.01f)
+            {
+                opening = false;
+                rb2d.gravityScale = -1f;
+                GetComponents<BoxCollider2D>()[0].enabled = false;
+                GetComponents<BoxCollider2D>()[1].enabled = false;
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        if (opening) {
+            posX = Mathf.SmoothDamp(door.localPosition.x, openPos, ref speed, 0.5f);
+        }
+        else
+        {
+            posX = Mathf.SmoothDamp(door.localPosition.x, closedPos, ref speed, 0.5f);
+        }
+        leftDoor.GetComponent<Transform>().localPosition = new Vector3(-posX, -0.2581787f, 0f);
+        rightDoor.GetComponent<Transform>().localPosition = new Vector3(posX, -0.2581787f, 0f);
+    }
+}
