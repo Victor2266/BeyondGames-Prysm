83a6de0ef950c2b73071bf4a69b9ab194c3a4586
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2022-01-06 02:14:03 -0500

    weapon system

---
+++ Assets/Scripts/DeactivateAfterSeconds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAfterSeconds : MonoBehaviour
+{
+
+    public float DeactivateAfterThisAmount;
+    private float timeStamp;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        timeStamp = Time.time + DeactivateAfterThisAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timeStamp)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -11,12 +11,28 @@ public class WeaponController : damageController
     private SpriteRenderer sprtrend;
 
     public float ReachLength;
-    public int DMG;
+    private int DMG;
+    public float DMG_Scaling;
     public float DMGTextSize;
+
+    private Vector3 lastPosition;
+    private float totalDistance;
+
+    private float timeStamp;
+    private float startTime;
+    public float activeTimeLimit;
+    public float cooldownTime;
+
+    private bool WeaponEnabled;
+
     // Start is called before the first frame update
     void Start()
     {
         sprtrend = GetComponent<SpriteRenderer>();
+        lastPosition = transform.position;
+        timeStamp = 0f;
+        startTime = 0f;
+        WeaponEnabled = false;
     }
 
     // Update is called once per frame
@@ -24,23 +40,40 @@ public class WeaponController : damageController
     {
         //transform.localPosition = whiteArrow.transform.localPosition;
         //transform.localRotation = whiteArrow.transform.localRotation;
-
         if (transform.localPosition.magnitude > ReachLength)
         {
             transform.localPosition = transform.localPosition.normalized * ReachLength;
         }
 
-        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(0) && timeStamp <= Time.time && !WeaponEnabled)
         {
             GetComponent<CapsuleCollider2D>().enabled = true;
             sprtrend.color = new Vector4(1f, 1f, 1f, 1f);
 
+            lastPosition = transform.position;
+            startTime = Time.time;
+            WeaponEnabled = true;
+
         }
-        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+        if (Input.GetMouseButton(0) && timeStamp <= Time.time && WeaponEnabled)
+        {
+            float distance = Vector3.Distance(lastPosition, transform.position);
+            totalDistance += distance;
+            lastPosition = transform.position;
+            DMG = (int)(totalDistance * DMG_Scaling);
+        }
+        if ( (Input.GetMouseButtonUp(0) && WeaponEnabled) || (Time.time > startTime + activeTimeLimit && WeaponEnabled) )
         {
             GetComponent<CapsuleCollider2D>().enabled = false;
             sprtrend.color = new Vector4(1f, 1f, 1f, 0.5f);
+
+            totalDistance = 0f;
+            timeStamp = Time.time + cooldownTime;
+
+            WeaponEnabled = false;
         }
+
+
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
@@ -48,32 +81,29 @@ public class WeaponController : damageController
         {
             GameObject gameObject = Instantiate(pop, collision.GetContact(0).point, transform.rotation);
         }
-
         if (collision.gameObject.tag == "enemy")
         {
-            collision.gameObject.SendMessage("TakeDamage", DMG);
-            ShowDMGText(DMG, DMGTextSize);
-            GameObject gameObject = Instantiate(pop, collision.GetContact(0).point, transform.rotation);
+            AttackEntity(1f, collision);
         }
-
         if (collision.gameObject.tag == "boss")
         {
-            collision.gameObject.SendMessage("TakeDamage", DMG / 2f);
-            ShowDMGText((DMG / 2), DMGTextSize);
+            AttackEntity(0.5f, collision);
         }
         if (collision.gameObject.tag == "CritBox")
         {
-            collision.gameObject.SendMessage("TakeDamage", DMG * 2);
-            ShowDMGText((DMG * 2), DMGTextSize * 2);
-        }
-        if (collision.gameObject.tag == "EnemyPlayer")
-        {
-            collision.gameObject.SendMessage("CmdTakeDamage", DMG);
-            ShowDMGText(DMG, DMGTextSize);
+            AttackEntity(2f, collision);
         }
         if (collision.gameObject.tag == "Player")
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.gameObject.GetComponent<CapsuleCollider2D>());
         }
+    }
+
+    private void AttackEntity(float multiplier, Collision2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", (int) (DMG * multiplier));
+        ShowDMGText((int)(DMG * multiplier), DMGTextSize);
+        GameObject gameObject = Instantiate(pop, collision.GetContact(0).point, transform.rotation);
+        totalDistance = 0f;
     }
 }
