818c51e67637b7b44c878eb4e0fd97b95c5152d3
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2022-10-13 03:26:22 -0400

    added font, heartbeat script, logo page

---
+++ Assets/OscillateXYZSize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillateXYZSize : MonoBehaviour
+{
+    public float startSize;
+    public float Speed;
+    public float Amplitude;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale = new Vector3(startSize + Mathf.Cos(Time.time * Speed) * Amplitude, startSize + Mathf.Cos(Time.time * Speed) * Amplitude, startSize + Mathf.Cos(Time.time * Speed) * Amplitude);
+    }
+}
