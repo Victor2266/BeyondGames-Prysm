28d83d7bdcda5d9d7693e536897f3788527643bd
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2022-11-17 15:17:36 -0500

    change prompt on enable

---
+++ Assets/ChangePromptOnEnable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangePromptOnEnable : MonoBehaviour
+{
+    [SerializeField]
+    public string[] Prompts;
+    public TextMeshProUGUI textMesh;
+
+
+    private void OnEnable()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh.text = Prompts[Random.Range(0, Prompts.Length)];
+    }
+}
