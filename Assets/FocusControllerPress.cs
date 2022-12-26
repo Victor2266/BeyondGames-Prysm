9ba5286511855a87cdb020ca89c01bf5f13e4c50
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2022-12-25 23:51:09 -0500

    added controller support to level select page

---
+++ Assets/FocusControllerPress.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class FocusControllerPress : MonoBehaviour
+{
+    public GameObject player;
+    public PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerInput = player.GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for controller press
+        if (playerInput.actions["Navigate"].WasPressedThisFrame() && (EventSystem.current.currentSelectedGameObject == null || !EventSystem.current.currentSelectedGameObject.active))
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            GameObject defaultMenuOption = GameObject.FindGameObjectWithTag("DefaultOption");
+            if (defaultMenuOption != null)
+                EventSystem.current.SetSelectedGameObject(defaultMenuOption);
+        }
+        else if (playerInput.actions["Point"].WasPerformedThisFrame())
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+}
