20650c721f7d5a9b25b50cfbb0da94b9332b8b36
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2022-12-27 03:14:20 -0500

    added version number to main menu 0.1x

---
+++ Assets/WriteVersionNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WriteVersionNumber : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = Application.version;
+    }
+}
