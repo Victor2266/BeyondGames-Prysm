986a512a6dc2083b29bd0878770c19211756d7b0
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-03-20 11:12:47 -0400

    changed level 2 tips

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -38,6 +38,7 @@ public partial class WeaponController : damageController
     private Equipment.ElementType activeElement;
 
     public Text DamageCounter;
+    public Text MaxDMGText;
 
     AudioSource audioSource;
 
