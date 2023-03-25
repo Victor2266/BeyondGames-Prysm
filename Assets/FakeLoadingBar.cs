45750c1a6bb0fe205baba315157e8b0036fc293a
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-03-25 05:00:09 -0400

    added fake loading bars

---
+++ Assets/FakeLoadingBar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeLoadingBar : MonoBehaviour
+{
+    public float endinglength = 1280f;
+    public float loadingTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scaleX(gameObject, endinglength, loadingTime).setEase(LeanTweenType.easeInOutExpo);
+    }
+}
