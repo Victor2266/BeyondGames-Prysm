9de8c7c858fd3ef3151b082b3e20a0131d21030a
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-03-31 00:54:00 -0400

    fade in and fade out text

--- Assets/Scripts/GrowthTimer.cs
+++ Assets/Scripts/GrowthTimer.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 
 public class GrowthTimer : MonoBehaviour
 {
+
+    public TMPro.TextMeshPro text;
+
+    public float ticks;
+    public float tickLimit;
+
+    private float startValue;
+    public bool grows;
+    public float EndSize = 1;
+
+    public float widthOfBullet;
+
+    public float timeOfBullet;
+
+    private float timeend;
+
+    private float endValue;
+
+    public bool diesRightAfter;
+
+    public bool affectZ;
+
+    public bool FadeInText;
+    private bool startFadingIn = false;
+
+    public Color EndColor;
+
     private void Start()
     {
+        if (FadeInText)
+        {
+            text = GetComponent<TMPro.TextMeshPro>();
+            EndColor = text.color;
+        }
+
         ticks = Time.time;
         timeend = Time.time + tickLimit;
         endValue = timeend - ticks;
@@ -18,6 +51,10 @@ public class GrowthTimer : MonoBehaviour
         {
             if (diesRightAfter)
             {
+                if (GetComponent<DeathTimer>().enabled == false)
+                {
+                    LeanTween.cancel(this.gameObject);
+                }
                 GetComponent<DeathTimer>().enabled = true;
             }
         }
@@ -46,25 +83,31 @@ public class GrowthTimer : MonoBehaviour
                 gameObject.transform.localScale = new Vector3(-currentSize, currentSize, 1f);
             }
         }
+        if (FadeInText)
+        {
+            if (!startFadingIn)
+            {
+                startFadingIn = true;
+                LeanTween.value(gameObject, updateTextColorCallback, Color.clear, EndColor, (tickLimit)).setEase(LeanTweenType.easeInOutSine);
+            }
+            /*if(Time.timeScale == 0)
+            {
+                LeanTween.pause(gameObject);
+            }
+            else
+            {
+                if (LeanTween.isPaused(gameObject))
+                {
+                    LeanTween.resume(gameObject);
+                }
+            }*/
+        }
+
     }
 
-    public float ticks;
-    public float tickLimit;
-
-    private float startValue;
-    public bool grows;
-    public float EndSize = 1;
-
-    public float widthOfBullet;
-
-    public float timeOfBullet;
-
-    private float timeend;
-
-    private float endValue;
-
-    public bool diesRightAfter;
-
-    public bool affectZ;
+    void updateTextColorCallback(Color val)
+    {
+        text.color = val;
+    }
 }
 
