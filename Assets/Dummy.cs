f4ca91c23a56099470f76bff0d39ec1673f45190
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-04-27 14:26:21 -0400

    added text to dummy

--- Assets/Dummy.cs
+++ Assets/Dummy.cs
@@ -7,19 +7,16 @@ public class Dummy : MonoBehaviour
 {
     public float totalDamage;
     public BloodSplatterer BSplat;
-    public TMPro.TextMeshProUGUI text;
+
+    public TMPro.TextMeshProUGUI totalDMGtext;
+    public TMPro.TextMeshProUGUI lastHittext;
 
     private void Start()
     {
         BSplat = GetComponent<BloodSplatterer>();
     }
-    public void FixedUpdate()
-    {
-        if(totalDamage > 0.01f)
-        {
-            totalDamage -= 0.01f;
-        }
-    }
+
+
     public void TakeDamage(float amount)
     {
         totalDamage += amount;
@@ -30,12 +27,17 @@ public class Dummy : MonoBehaviour
     }
     public void UpdateHealthText(float amount)
     {
-        LeanTween.cancel(text.gameObject);
+        LeanTween.cancel(lastHittext.gameObject);
+        LeanTween.cancel(totalDMGtext.gameObject);
+
         LTSeq sequence = LeanTween.sequence();
-        sequence.append(LeanTween.scale(text.gameObject, Vector3.one * 0.3f + Vector3.one * amount / 70f, 0.1f).setEaseInOutBounce());
-        sequence.append(LeanTween.scale(text.gameObject, Vector3.one * 0.3f, 0.3f).setEaseInOutCubic());
-        text.color = Color.Lerp(Color.white, Color.red, amount / 100f);
-        text.text = Mathf.Round(amount).ToString();
+        sequence.append(LeanTween.scale(lastHittext.gameObject, Vector3.one * 0.3f + Vector3.one * amount / 70f, 0.1f).setEaseInOutBounce());
+        sequence.append(LeanTween.scale(lastHittext.gameObject, Vector3.one * 0.3f, 0.3f).setEaseInOutCubic());
+
+        lastHittext.color = Color.Lerp(Color.white, Color.red, amount / 100f);
+        lastHittext.text = Mathf.Round(amount).ToString();
+
+        totalDMGtext.text = "Total Damage: " + Mathf.Round(totalDamage).ToString();
     }
 
 }
