7763789880a0cb9b4999e1e66b73528ee2fa2d78
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-04-29 23:06:10 -0400

    win button and estoc

---
+++ Assets/WinButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinButton : MonoBehaviour
+{
+    public TrainingRoomManager manager;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            LeanTween.cancel(gameObject);
+
+            LeanTween.moveY(gameObject, -2.25f, 1f).setEaseInOutExpo().setOnComplete(manager.WinLevel);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            LeanTween.cancel(gameObject);
+            LeanTween.moveY(gameObject, -1.75f, 1f).setEaseInOutExpo();
+        }
+    }
+}
