0a591e6fb3a0c7cba8ce9b66fc6f3bb3919277b4
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-05-04 01:35:41 -0400

    barrel moves now

---
+++ Assets/GunBarrelMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBarrelMovement : MonoBehaviour
+{
+    public GameObject gunSlide;
+    public GameObject barrelPivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zrot = (gunSlide.transform.position.x - 2.21f) / (3.75f - 2.21f) * -5f;
+        barrelPivot.transform.localEulerAngles = new Vector3(0f, 0f, zrot);
+    }
+}
