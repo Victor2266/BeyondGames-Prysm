ae41f3e31d29c5909c918171239f5968a1322797
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-05-03 22:26:19 -0400

    sounds and level 3 opening

---
+++ Assets/GunMagMinigame.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(MeshCollider))]
+public class GunMagMinigame : MonoBehaviour
+{
+    public bool mouseHovering;
+    public PlayerInput playerInput;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Vector3 velocity;
+    private Vector2 joystickPos;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DialogManager.instance.index == 8)
+        {
+            if (playerInput.actions != null)
+                joystickPos = playerInput.actions["Navigate"].ReadValue<Vector2>();
+
+            if (-9.175785f < transform.localPosition.x && transform.localPosition.x < 8.549582f && -25.27896f < transform.localPosition.y && transform.localPosition.y < -13.75938f)
+                transform.position += (Vector3)joystickPos * 0.05f;
+            else
+            {
+                LeanTween.cancel(gameObject);
+                LeanTween.moveLocal(gameObject, new Vector3(-5.655519f, -22f), 1f).setEaseOutExpo();
+            }
+
+            if (3.137996f < transform.localPosition.x)
+            {
+                DialogManager.instance.index++;
+            }
+        }
+        else if (DialogManager.instance.index == 9)
+        {
+            LeanTween.moveLocal(gameObject, new Vector3(4.288f, -22.68f, 0f), 1f).setEaseOutExpo();
+            DialogManager.instance.index++;
+        }
+        else if (DialogManager.instance.index == 10)
+        {
+            if (playerInput.actions != null)
+                joystickPos = playerInput.actions["Navigate"].ReadValue<Vector2>();
+
+            if (-25.27896f < transform.localPosition.y && transform.localPosition.y < -13.75938f)
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + joystickPos.y * 0.05f, 0f);
+            else
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, -25.27896f, 0f);
+            }
+
+            if(transform.localPosition.y > -19.49504f)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, -19.49504f, 0f);
+
+                DialogManager.instance.index++;
+            }
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        if (DialogManager.instance.index == 8 || DialogManager.instance.index == 10)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+            transform.localPosition = Vector3.SmoothDamp(transform.localPosition, curPosition, ref velocity, 0.2f);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
---
+++ Assets/Level3OpeningScene.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level3OpeningScene : DialogTrigger
+{
+    public GameObject mainCamera; //start at -10, go to -2 pause 
+    public GameObject FadeToBlack;
+    public Button NextButton;
+    public Button SkipButton;
+
+    public AudioSource audioSource;
+    public AudioClip CutSceneSong;
+
+    public AudioSource[] Sounds;
+
+    private bool notTriggeredYet;
+
+    public GameObject gunMag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        notTriggeredYet = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DialogManager.instance.index == 0)
+        {
+            TriggerDialog();
+
+        }
+        if (DialogManager.instance.index == 2)//hahaha, element 1
+        {
+            if(!Sounds[0].isPlaying)
+                Sounds[0].Play();
+        }
+        if (DialogManager.instance.index == 3)//tsktsk, ele 2
+        {
+            if (!Sounds[1].isPlaying)
+                Sounds[1].Play();
+        }
+        if (DialogManager.instance.index == 4)//dadun, ele 3
+        {
+            if (!Sounds[2].isPlaying)
+                Sounds[2].Play();
+            DialogManager.instance.index++;
+            LeanTween.moveY(mainCamera, -10f, 1f).setEaseOutExpo();
+        }
+        if (DialogManager.instance.index == 6)//dadun,ele 4
+        {
+            LeanTween.moveY(mainCamera, -19.5f, 1f).setEaseOutExpo();
+            if (DialogManager.instance.animator.GetBool("IsOpen") == false)
+            {
+                DialogManager.instance.index = 7;
+            }
+        }
+        if (DialogManager.instance.index == 7)//gun minigame
+        {
+            LeanTween.moveLocalY(gunMag, -22f, 1f).setEaseOutExpo();
+            DialogManager.instance.index++;
+        }
+        if (DialogManager.instance.index == 11)//click sound
+        {
+            Sounds[3].Play();
+            DialogManager.instance.index++;
+        }
+
+    }
+}
