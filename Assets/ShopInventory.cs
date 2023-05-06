d7eded285035672c359ab71830541aead1caa468
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-05-05 22:41:19 -0400

    items show up in shop

---
+++ Assets/ShopInventory.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopInventory : MonoBehaviour
+{
+	private void Start()
+	{
+		if (items == null)
+		{
+			items = new List<Item>();
+		}
+		if (onItemChangedCallback != null)
+			onItemChangedCallback.Invoke();
+	}
+	// Callback which is triggered when
+	// an item gets added/removed.
+	public delegate void OnItemChanged();
+	public OnItemChanged onItemChangedCallback;
+
+	public int space = 99;  // Amount of slots in inventory
+
+	// Current list of items in inventory
+	public List<Item> items = new List<Item>();
+
+	// Add a new item. If there is enough room we
+	// return true. Else we return false.
+	public bool Add(Item item)
+	{
+		// Don't do anything if it's a default item
+		if (!item.isDefaultItem)
+		{
+			// Check if out of space
+			if (items.Count >= space)
+			{
+				Debug.Log("Not enough room.");
+				return false;
+			}
+
+			if (item is ConsumableItem)
+			{
+				int duplicates = 0;
+				foreach (Item currentItem in items)
+				{
+					if (currentItem.name == item.name)//check for duplicates in inventory
+					{
+						duplicates++;
+					}
+				}
+
+				if (duplicates < ((ConsumableItem)item).maxStacks)
+				{
+					items.Add(item);    // Add item to list
+
+					// Trigger callback
+					if (onItemChangedCallback != null)
+						onItemChangedCallback.Invoke();
+				}
+			}
+
+			foreach (Item currentItem in items)
+			{
+				if (currentItem.name == item.name)//check for duplicates in inventory
+				{
+					return true;
+				}
+			}
+			foreach (Equipment currentEquips in EquipmentManager.instance.getCurrentEquipment())
+			{
+				if (currentEquips != null)
+				{
+					if (currentEquips.name == item.name)//check for duplicates in equipped items
+					{
+						return true;
+					}
+				}
+			}
+
+
+			items.Add(item);    // Add item to list
+
+			// Trigger callback
+			if (onItemChangedCallback != null)
+				onItemChangedCallback.Invoke();
+		}
+
+		return true;
+	}
+
+	// Remove an item
+	public void Remove(Item item)
+	{
+		items.Remove(item);     // Remove item from list
+
+		// Trigger callback
+		if (onItemChangedCallback != null)
+			onItemChangedCallback.Invoke();
+	}
+
+}
---
+++ Assets/ShopInventoryUI.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+/* This object updates the inventory UI. */
+
+public class ShopInventoryUI : MonoBehaviour
+{
+
+	public Transform itemsParent;   // The parent object of all the items
+	public GameObject inventoryUI;  // The entire UI
+	public GameObject emptySlot;
+
+	ShopInventory shop_inventory;    // Our current inventory
+	Inventory player_inventory;    // Our current inventory
+
+	InventorySlot[] slots;  // List of all the slots
+	public Image[] Tabs = new Image[2];
+	PlayerEntity player;
+	public PlayerInput playerInput;
+
+
+	public enum WeaponTypes { All, Weapons, Spells, Other };
+	public String CurrentTab;
+
+	private Color GreyedColor = new Color(1f, 1f, 1f, 0.5f);
+	private Color SelectedColor = new Color(1f, 1f, 1f, 1f);
+
+	private void OnEnable()
+	{
+		if (Inventory.instance != null)
+		{
+			player_inventory = Inventory.instance;
+			player_inventory.onItemChangedCallback += UpdateUI;    // Subscribe to the onItemChanged callback
+		}
+		shop_inventory = GetComponent<ShopInventory>();
+	}
+	void Start()
+	{
+		if (Inventory.instance != null)
+		{
+			player_inventory = Inventory.instance;
+			player_inventory.onItemChangedCallback += UpdateUI;    // Subscribe to the onItemChanged callback
+		}
+		shop_inventory = GetComponent<ShopInventory>();
+
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerEntity>();
+		playerInput = player.GetComponent<PlayerInput>();
+		// Populate our slots array
+		slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+	}
+
+	void Update()
+	{
+
+	}
+	public void CloseInventory()
+	{
+		Resume();
+		inventoryUI.SetActive(false);
+	}
+	private void Pause()
+	{
+		playerInput.actions.FindActionMap("UI").Enable();
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+		player.mousePointer.SetActive(false);
+		player.GetComponent<PlayerController>().enabled = false;
+		Time.timeScale = 0f;
+		PauseMenuScript.isPaused = true;
+
+	}
+
+	private void Resume()
+	{
+		playerInput.SwitchCurrentActionMap("Player");
+		Cursor.lockState = CursorLockMode.Confined;
+		if (DialogManager.instance != null)
+		{
+			if (!DialogManager.instance.isDisplayingDialog)
+				Cursor.visible = false;
+		}
+		else
+		{
+			Cursor.visible = false;
+		}
+		if (player.GetComponent<PlayerEntity>().weapon > 0)
+		{
+			player.mousePointer.SetActive(true);
+		}
+		else
+		{
+			player.mousePointer.SetActive(false);
+		}
+		player.GetComponent<PlayerController>().enabled = true;
+		Time.timeScale = 1f;
+		PauseMenuScript.isPaused = false;
+	}
+
+	// Update the inventory UI by:
+	//		- Adding items
+	//		- Clearing empty slots
+	// This is called using a delegate on the Inventory.
+	void UpdateUI()
+	{
+		// Loop through all the slots
+		if (itemsParent == null)
+			return;
+		slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+		if(CurrentTab == "Shop")
+        {
+			while (slots.Length < shop_inventory.items.Count)
+			{
+				var newSlot = Instantiate(emptySlot, itemsParent);//new slot for adding item to UI
+				slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+			}
+			for (int i = 0; i < slots.Length; i++)
+			{
+
+				if (i < shop_inventory.items.Count)  // If there is an item to add
+				{
+					slots[i].AddItem(shop_inventory.items[i]);   // Add it
+				}
+				else
+				{
+					// Otherwise clear the slot
+					slots[i].ClearSlot();
+					Destroy(slots[i].gameObject);
+				}
+			}
+		}
+        else
+        {
+			while (slots.Length < player_inventory.items.Count)
+			{
+				var newSlot = Instantiate(emptySlot, itemsParent);//new slot for adding item to UI
+				slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+			}
+			for (int i = 0; i < slots.Length; i++)
+			{
+
+				if (i < player_inventory.items.Count)  // If there is an item to add
+				{
+					slots[i].AddItem(player_inventory.items[i]);   // Add it
+				}
+				else
+				{
+					// Otherwise clear the slot
+					slots[i].ClearSlot();
+					Destroy(slots[i].gameObject);
+				}
+			}
+		}
+
+	}
+
+	public void SetCurrentTab(int WeapType)
+	{
+		switch (WeapType)
+		{
+			case 1:
+				CurrentTab = "Shop";
+				Tabs[1].color = GreyedColor;
+				Tabs[0].color = SelectedColor;
+				break;
+			case 2:
+				CurrentTab = "Inventory";
+				Tabs[0].color = GreyedColor;
+				Tabs[1].color = SelectedColor;
+				break;
+		}
+		Debug.Log("Shop tab: " + CurrentTab);
+		UpdateUI();
+
+
+	}
+}
